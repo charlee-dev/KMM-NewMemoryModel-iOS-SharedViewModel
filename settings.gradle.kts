aac48f0a7b62eabcc17bcfee73957c26c00a89a9
Adrian Witaszak <adrianwitaszak@gmail.com>
2022-12-06 09:57:36 +0000

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM-NewMemoryModel-iOS-SharedViewModel"
+include(":androidApp")
+include(":shared")
